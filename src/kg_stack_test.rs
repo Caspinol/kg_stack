dc069b54ae1cfd8ac94861992d7642073c022394
Caspinol <kris@catdamnit.net>
2016-09-08 00:03:03 +0100

    Options not Result

--- src/kg_stack_test.rs
+++ src/kg_stack_test.rs
@@ -1,7 +1,6 @@
 #[cfg(test)]
 
 use super::*;
-use StackStuff;
 
 #[test]
 fn create_new_stack_object(){
@@ -16,7 +15,20 @@ fn push_stack_test(){
     stack.push("Sraka".to_string());
 
     assert!(stack.size() == 2);
-    assert!(stack.pop() == Ok("Sraka".to_string()));
-    assert!(stack.pop() == Ok("Dupa".to_string()));
-    assert!(stack.pop() == Err(-1));
+    assert!(stack.pop() == Some("Sraka".to_string()));
+    assert!(stack.size() == 1);
+    assert!(stack.pop() == Some("Dupa".to_string()));
+    assert!(stack.size() == 0);
+    assert!(stack.pop() == None);
+}
+
+#[test]
+fn peek_stack_test(){
+    let mut stack = KGStack::<String>::new();
+    stack.push("Dupa".to_string());
+    stack.push("Sraka".to_string());
+
+    assert!(stack.size() == 2);
+    assert!(stack.peek() == Some(&"Sraka".to_string()));
+    assert!(stack.size() == 2);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,6 @@ Rust test
 
 mod kg_stack_test;
 use std::vec::Vec;
-use std::result::Result;
 
 pub struct KGStack<T> {
     list: Vec<T>,
@@ -14,7 +13,9 @@ pub struct KGStack<T> {
 
 pub trait StackStuff<T> {
     fn push(&mut self, x: T);
-    fn pop(&mut self) -> Result<T,i8>;
+    fn pop(&mut self) -> Option<T>
+;
+    fn peek(&self) -> Option<&T>;
     fn size(&self) -> usize;
 }
 
@@ -24,11 +25,12 @@ impl<T> StackStuff<T> for KGStack<T> {
         self.list.push(x);
     }
 
-    fn pop(&mut self) -> Result<T,i8> {
-        match self.list.is_empty() {
-            true => Err(-1),
-            false => Ok(self.list.pop().unwrap())
-        }
+    fn pop(&mut self) -> Option<T> {
+        self.list.pop()
+    }
+
+    fn peek(&self) -> Option<&T>{
+        self.list.get(self.list.len()-1)
     }
     
     fn size(&self) -> usize {   
